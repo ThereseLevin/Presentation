360b18117c368745377f04ea058bdf9f4d785cc0
Therese Levin <therese.levin@outlook.com>
2018-11-12 17:58:32 +0100

    Js comments added

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,24 +1,32 @@
+/*
+ * MAIN js file for slider
+ */
+
+/* Variables with the element for the arrows */ 
 let arrow1 = document.querySelector('.arrow1');
 let arrow2 = document.querySelector('.arrow2');
-let arrow3 = document.querySelector('.arrow3');
+let arrow3 = document.querySelector('.arrowBack3');
 
+/* Functions for switching to the right with arrows */
 function first_arrow() { 
-    arrow1.parentElement.className = 'none';
-    arrow2.parentElement.className = 'block';
+    arrow1.parentElement.classList.add("none");
+    arrow2.parentElement.classList.add("block");
 }
 
 function second_arrow() {
-    arrow2.parentElement.className = 'none';
-    arrow3.parentElement.className = 'block';
+    arrow2.parentElement.classList.remove("block");
+    arrow3.parentElement.classList.add("block");
 }
 
+/* Functions for switching to the left with arrows */
+
 function second_arrow_back() {
-    arrow2.parentElement.className = 'none';
-    arrow1.parentElement.className = 'block';
+    arrow2.parentElement.classList.remove("block");
+    arrow1.parentElement.classList.remove("none");
 }
 
 function third_arrow_back() {
-    arrow2.parentElement.className = 'block';
-    arrow3.parentElement.className = 'none';
+    arrow2.parentElement.classList.add("block");
+    arrow3.parentElement.classList.remove("block");
 }
 
