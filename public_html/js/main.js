1b274cec2d4cf3e41696e28be15740c638eed6f7
Therese Levin <therese.levin@outlook.com>
2018-11-14 11:26:15 +0100

    slide 2 content added & setInterval on H2

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,17 +1,17 @@
-/*
- * MAIN js file for slider
- */
+/* MAIN js file for slider */
 
 /* Variables with the element for the arrows */ 
 let arrow1 = document.querySelector('.arrow1');
 let arrow2 = document.querySelector('.arrow2');
 let arrow3 = document.querySelector('.arrowBack3');
 let h3 = document.querySelector("h3");
+let slide2H2 = document.querySelector(".slide2H2");
+let IE = document.querySelector(".IE");
 
 /* Functions for switching to the right with arrows */
 function first_arrow() { 
     arrow1.parentElement.classList.add("none");
-    arrow2.parentElement.classList.add("block");
+    arrow2.parentElement.classList.add("block"); 
 }
 
 function second_arrow() {
@@ -31,8 +31,21 @@ function third_arrow_back() {
     arrow3.parentElement.classList.remove("block");
 }
 
+
+
 /* Eventlisternes */
 
 h3.addEventListener("click", function(){
     h3.classList.add("colorChange");
 });
+
+slide2H2.addEventListener("click", function(){
+    slide2H2.classList.add("colorChange");
+});
+
+/* Set Intervals */
+
+setInterval(function addIE(){
+    IE.classList.add("block");
+    IE.classList.remove("none");
+    }, 8000);
